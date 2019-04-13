68686d77ab126560d658fae444cef254c55656bb
Valery Kostikov <mkoctuk@gmail.com>
2019-04-13 22:25:43 +0300

    add postgres

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use actix_web::{http, server, App, HttpRequest, Responder};
+use postgres::TlsMode;
+use r2d2::Pool;
+use r2d2_postgres::PostgresConnectionManager;
+
+#[derive(Clone)]
+struct State {
+    db: Pool<PostgresConnectionManager>,
+}
 
 fn index(req: &HttpRequest) -> impl Responder {
     let headers = req.headers();
@@ -37,11 +45,30 @@ fn index(req: &HttpRequest) -> impl Responder {
     )
 }
 
+fn index1(req: &HttpRequest<State>) {
+    let pool = req.state().db.get().unwrap();
+    // let c = Contact::get(&pool, 2).unwrap();
+    // let data = JsonData {
+    //     data: c,
+    //     error: None,
+    //     ok: true,
+    // };
+    // // println!("Request number: {}\n{:?}", c.id, c.name);
+    // HttpResponse::Ok().json(data)
+}
+
 fn main() {
+    let db_uri = dotenv::var("db_uri").expect("No found variable c_uri like postgres://postgres@localhost:5433 in environment");
+    let manager = PostgresConnectionManager::new(
+        db_uri.parse().unwrap(),
+        NoTls,
+    );
+    let pool = Pool::new(manager).unwrap();
+    let state = State { db: pool.clone() };
     let addr =
         dotenv::var("s_addr").expect("No found variable s_addr like 127.0.0.1:10000 in environment");
     let path = dotenv::var("s_path").expect("No found variable s_path like /checkpath in environment");
-    server::new(move || App::new().resource(&path, |r| r.method(http::Method::GET).f(index)))
+    server::new(move || App::with_state(state.clone()).resource(&path, |r| r.method(http::Method::GET).f(index)).resource("/c1", |r| r.method(http::Method::GET).f(index1)))
         .bind(addr)
         .unwrap()
         .run();
