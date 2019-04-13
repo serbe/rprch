68ebb7588b7d52cd8c7265a1c470fe467919a2c0
Valeriy Kostikov <mkoctuk@gmail.com>
2019-04-13 18:51:36 +0300

    use env for server

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
-extern crate actix_web;
-
 use actix_web::{http, server, App, HttpRequest, Responder};
 
 fn index(req: &HttpRequest) -> impl Responder {
     let headers = req.headers();
     [
-        "HTTP_VIA",
-        "HTTP_X_FORWARDED_FOR",
-        "HTTP_FORWARDED_FOR",
-        "HTTP_X_FORWARDED",
-        "HTTP_FORWARDED",
-        "HTTP_CLIENT_IP",
-        "HTTP_FORWARDED_FOR_IP",
-        "VIA",
-        "X_FORWARDED_FOR",
-        "FORWARDED_FOR",
-        "X_FORWARDED",
-        "FORWARDED",
         "CLIENT_IP",
+        "FORWARDED",
+        "FORWARDED_FOR",
         "FORWARDED_FOR_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED_FOR_IP",
         "HTTP_PROXY_CONNECTION",
+        "HTTP_VIA",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_FORWARDED_FOR",
+        "VIA",
+        "X_FORWARDED",
+        "X_FORWARDED_FOR",
     ]
     .iter()
     .fold(
         format!("<p>RemoteAddr: {}</p>\r\n", req.peer_addr().unwrap()),
         |mut acc, h| {
             if headers.contains_key(*h) {
-                acc.push_str(&format!("<p>{}: {:?}</p></p>\r\n", h, headers.get(*h).unwrap()));
+                acc.push_str(&format!(
+                    "<p>{}: {:?}</p></p>\r\n",
+                    h,
+                    headers.get(*h).unwrap()
+                ));
                 acc
             } else {
                 acc
@@ -36,8 +38,11 @@ fn index(req: &HttpRequest) -> impl Responder {
 }
 
 fn main() {
-    server::new(|| App::new().resource("/check", |r| r.method(http::Method::GET).f(index)))
-        .bind("5.39.102.29:16016")
+    let addr =
+        dotenv::var("s_addr").expect("No found variable s_addr like 127.0.0.1:10000 in environment");
+    let path = dotenv::var("s_path").expect("No found variable s_path like /checkpath in environment");
+    server::new(move || App::new().resource(&path, |r| r.method(http::Method::GET).f(index)))
+        .bind(addr)
         .unwrap()
         .run();
 }
