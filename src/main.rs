a54ef27710ddc34fae1ebeccf40fbbfb149b0d2a
Valeriy Kostikov <mkoctuk@gmail.com>
2019-04-14 07:44:13 +0300

    fix () for state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use actix_web::{http, server, App, HttpRequest, Responder};
-use postgres::TlsMode;
 use r2d2::Pool;
-use r2d2_postgres::PostgresConnectionManager;
+use r2d2_postgres::{PostgresConnectionManager, TlsMode};
 
 #[derive(Clone)]
 struct State {
     db: Pool<PostgresConnectionManager>,
 }
 
-fn index(req: &HttpRequest) -> impl Responder {
+fn index1(req: &HttpRequest<State>) -> impl Responder {
     let headers = req.headers();
     [
         "CLIENT_IP",
@@ -45,8 +44,8 @@ fn index(req: &HttpRequest) -> impl Responder {
     )
 }
 
-fn index1(req: &HttpRequest<State>) {
-    let pool = req.state().db.get().unwrap();
+fn index2(req: &HttpRequest<State>) -> impl Responder {
+    let _pool = req.state().db.get().unwrap();
     // let c = Contact::get(&pool, 2).unwrap();
     // let data = JsonData {
     //     data: c,
@@ -55,21 +54,27 @@ fn index1(req: &HttpRequest<State>) {
     // };
     // // println!("Request number: {}\n{:?}", c.id, c.name);
     // HttpResponse::Ok().json(data)
+    format!("index1")
 }
 
 fn main() {
-    let db_uri = dotenv::var("db_uri").expect("No found variable c_uri like postgres://postgres@localhost:5433 in environment");
-    let manager = PostgresConnectionManager::new(
-        db_uri.parse().unwrap(),
-        NoTls,
-    );
+    let db_uri = dotenv::var("db_uri")
+        .expect("No found variable db_uri like postgres://postgres@localhost:5433 in environment");
+    let manager = PostgresConnectionManager::new(db_uri, TlsMode::None).unwrap();
     let pool = Pool::new(manager).unwrap();
     let state = State { db: pool.clone() };
-    let addr =
-        dotenv::var("s_addr").expect("No found variable s_addr like 127.0.0.1:10000 in environment");
-    let path = dotenv::var("s_path").expect("No found variable s_path like /checkpath in environment");
-    server::new(move || App::with_state(state.clone()).resource(&path, |r| r.method(http::Method::GET).f(index)).resource("/c1", |r| r.method(http::Method::GET).f(index1)))
-        .bind(addr)
-        .unwrap()
-        .run();
+    let i2_path =
+        dotenv::var("i2_path").expect("No found variable i2_path like /checkpath in environment");
+    let s_addr = dotenv::var("s_addr")
+        .expect("No found variable s_addr like 127.0.0.1:10000 in environment");
+    let i1_path =
+        dotenv::var("i1_path").expect("No found variable i1_path like /checkpath in environment");
+    server::new(move || {
+        App::with_state(state.clone())
+            .resource(&i1_path, |r| r.method(http::Method::GET).f(index1))
+            .resource(&i2_path, |r| r.method(http::Method::GET).f(index2))
+    })
+    .bind(s_addr)
+    .unwrap()
+    .run();
 }
