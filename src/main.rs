1e8371df75b9b14a7e9acc0ad94f18eae29a6c2f
Valery Kostikov <mkoctuk@gmail.com>
2019-04-15 19:18:36 +0300

    add anon

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn check(req: &HttpRequest<State>) -> Result<String> {
                 format!("<p>RemoteAddr: {}</p>\r\n", peer_addr),
                 |mut acc, h| match headers.get(*h) {
                     Some(key) => {
-                        acc.push_str(&format!("<p>{}: {:?}</p></p>\r\n", h, key));
+                        acc.push_str(&format!("<p>{}: {:?}</p>\r\n", h, key));
                         acc
                     }
                     None => acc,
@@ -45,7 +45,7 @@ fn check(req: &HttpRequest<State>) -> Result<String> {
             );
             Ok(res)
         }
-        None => Ok("no parse peer addr".to_string())
+        None => Ok("no parse peer addr".to_string()),
     }
 }
 
@@ -56,6 +56,25 @@ fn proxy(req: &HttpRequest<State>) -> Result<String> {
     if let Ok(client) = req.state().db.get() {
         if let Ok(rows) = &client.query(
             "SELECT hostname FROM proxies TABLESAMPLE SYSTEM(1) WHERE work = true LIMIT 1",
+            &[],
+        ) {
+            for row in rows {
+                pr = Proxy {
+                    hostname: row.get(0),
+                }
+            }
+        }
+    };
+    Ok(pr.hostname)
+}
+
+fn anon_proxy(req: &HttpRequest<State>) -> Result<String> {
+    let mut pr = Proxy {
+        hostname: String::new(),
+    };
+    if let Ok(client) = req.state().db.get() {
+        if let Ok(rows) = &client.query(
+            "SELECT hostname FROM proxies TABLESAMPLE SYSTEM(1) WHERE anon = true AND work = true LIMIT 1",
             &[],
         ) {
             for row in rows {
@@ -88,25 +107,56 @@ fn proxy_with_scheme(req: &HttpRequest<State>) -> Result<String> {
     Ok(pr.hostname)
 }
 
+fn anon_proxy_with_scheme(req: &HttpRequest<State>) -> Result<String> {
+    let mut pr = Proxy {
+        hostname: String::new(),
+    };
+    let scheme = &req.match_info()["scheme"];
+    if let Ok(client) = req.state().db.get() {
+        if let Ok(rows) = &client.query(
+            "SELECT hostname FROM proxies TABLESAMPLE SYSTEM(1) WHERE anon = true AND work = true AND scheme = $1 LIMIT 1",
+            &[&scheme],
+        ) {
+            for row in rows {
+                pr = Proxy {
+                    hostname: row.get(0),
+                }
+            }
+        }
+    };
+    Ok(pr.hostname)
+}
+
 fn main() {
     let db_uri = dotenv::var("db_uri")
         .expect("No found variable db_uri like postgres://postgres@localhost:5433 in environment");
     let manager = PostgresConnectionManager::new(db_uri, TlsMode::None).unwrap();
     let pool = Pool::new(manager).unwrap();
     let state = State { db: pool.clone() };
-    let proxy_path =
-        dotenv::var("proxy_path").expect("No found variable proxy_path like /proxypath in environment");
+    let proxy_path = dotenv::var("proxy_path")
+        .expect("No found variable proxy_path like /proxypath in environment");
     let s_addr = dotenv::var("s_addr")
         .expect("No found variable s_addr like 127.0.0.1:10000 in environment");
     let check_path = dotenv::var("check_path")
         .expect("No found variable check_path like /checkpath in environment");
+    let anon_proxy_path = format!("{}/anon", proxy_path);
     let proxy_path_with_scheme = format!("{}/{{scheme}}", proxy_path);
+    let anon_proxy_path_with_scheme = format!("{}/anon/{{scheme}}", proxy_path);
     server::new(move || {
         App::with_state(state.clone())
             .resource(&check_path, |r| r.method(http::Method::GET).f(check))
             .resource(&proxy_path, |r| r.method(http::Method::GET).f(proxy))
-            .resource(&proxy_path_with_scheme, |r| r.method(http::Method::GET).f(proxy_with_scheme))
+            .resource(&anon_proxy_path, |r| {
+                r.method(http::Method::GET).f(anon_proxy)
+            })
+            .resource(&proxy_path_with_scheme, |r| {
+                r.method(http::Method::GET).f(proxy_with_scheme)
+            })
+            .resource(&anon_proxy_path_with_scheme, |r| {
+                r.method(http::Method::GET).f(anon_proxy_with_scheme)
+            })
     })
+    .keep_alive(server::KeepAlive::Timeout(10))
     .bind(s_addr)
     .unwrap()
     .run();
